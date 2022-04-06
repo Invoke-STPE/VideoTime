5521036401361d67822ca73103fd18caa22afa48
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-04-06 20:29:22 +0200

    Updated SRP
    Tried to apply the Single Reponsibility Prinicple.

---
+++ VideoTime.UI/Helpers/PathHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace VideoTime.UI.Helpers
+{
+    public static class PathHelper
+    {
+
+        public static List<string> ResolveDirectoriesPath(string path)
+        {
+            string directoryPath = ResolveAbsolutePath(path);
+
+            return GetAllDirectoryPathsFromFolder(directoryPath);
+
+        }
+        private static string ResolveAbsolutePath(string cmdArgument)
+        {
+            return Path.GetFullPath(cmdArgument);
+        }
+        private static List<string> GetAllDirectoryPathsFromFolder(string parentFolder)
+        {
+            List<string> directories = Directory.GetDirectories(parentFolder, "*", searchOption: SearchOption.AllDirectories).ToList();
+            directories.Add(parentFolder);
+            return directories;
+        }
+    }
+}
--- VideoTime.UI/Program.cs
+++ VideoTime.UI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Shell32;
+using VideoTime.UI.Helpers;
 
 
 namespace VideoTime.UI
@@ -12,50 +13,16 @@ namespace VideoTime.UI
         [STAThread]
         static void Main(string[] args)
         {
-            // Read CMD Input
-            string filePath = Path.GetFullPath(args[0]);
-
             // Define types needed.
-            TimeSpan duration = default;
-            List<string> filePaths = default;
-            // Create a new shell object
-            Shell shell = new Shell();
 
-            try
-            {
-                filePaths = GetAllDirectoriesFromFolder(filePath);
-            }
-            catch (DirectoryNotFoundException directoryNotFound)
-            {
-                Console.WriteLine(directoryNotFound.Message);
-                return;
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception.Message);
-            }
+            List<string> filePaths = PathHelper.ResolveDirectoriesPath(args[0]);
+
+            TimeSpan duration = DurationHelper.GetDurationOfVideoFiles(filePaths);
 
 
-            foreach (string path in filePaths)
-            {
-                // http://www.levibotelho.com/development/get-the-length-of-a-video-in-c/
-                Folder folder = shell.NameSpace(path);
-                foreach (FolderItem2 item in folder.Items())
-                {
-                    
-                    if (item.Name.EndsWith(".mp4"))
-                    {
-                        duration += TimeSpan.FromSeconds(item.ExtendedProperty("System.Media.Duration") / 10000000);
-                    }
-                }
-            }
+
             Console.WriteLine($"The total video duration for the path is: {duration}");
         }
-        public static List<string> GetAllDirectoriesFromFolder(string parentFolder)
-        {
-            List<string> directories = Directory.GetDirectories(parentFolder, "*", searchOption: SearchOption.AllDirectories).ToList();
-            directories.Add(parentFolder);
-            return directories;
-        }
+       
     }
 }
