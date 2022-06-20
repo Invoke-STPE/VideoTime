1409a35755a17e39d7d892377ac4dba7bd3bc88b
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-06-20 23:05:11 +0200

    Added GUI.
    Updated VideoTime to use a GUI.

---
+++ VideoTime.GUI/Helpers/PathHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace VideoTime.GUI.Helpers
+{
+    public static class PathHelper
+    {
+        public static List<string> ResolveDirectoriesPath(string path)
+        {
+            List<string> test = GetAllDirectoryPathsFromFolder(path);
+            return test;
+
+        }
+
+        // Have to loop through the return of GetDirectories, else I cannot continue if an exception is hit.
+        // http://www.blackwasp.co.uk/FolderRecursion.aspx
+        private static List<string> GetAllDirectoryPathsFromFolder(string path)
+        {
+            List<string> directoryPaths = new List<string>();
+            try
+            {
+                foreach (string folder in Directory.GetDirectories(path))
+                {
+                    Console.WriteLine($"{folder}");
+                    directoryPaths.Add(folder);
+                    GetAllDirectoryPathsFromFolder(folder);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine($"Unauthorized Access found");
+            }
+            return directoryPaths;
+        }
+    }
+}
---
+++ VideoTime.GUI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VideoTime.GUI.Helpers;
+
+namespace VideoTime.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // TODO: Need a stackpanel to show folders iterated, and unauthorized folders
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.ShowDialog();
+                pathText.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void GetDuration_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> filePaths = PathHelper.ResolveDirectoriesPath(pathText.Text);
+
+            TimeSpan duration = DurationHelper.GetDurationOfVideoFiles(filePaths);
+
+            resultText.Text = $"The folder contains {Math.Floor( duration.TotalHours )} Hours of video.";
+
+        }
+    }
+}
