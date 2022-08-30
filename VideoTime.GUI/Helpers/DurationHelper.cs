178e981498f9b2faaa9be50574498db81f04c9a6
Steven Exaudi Basker Pedersen <spe@commentor.dk>
2022-08-30 11:05:51 +0200

    Clean up.
    Removed commented code.

--- VideoTime.GUI/Helpers/DurationHelper.cs
+++ VideoTime.GUI/Helpers/DurationHelper.cs
@@ -7,25 +7,7 @@ namespace VideoTime.GUI.Helpers
 {
     public static class DurationHelper
     {
-        //public static TimeSpan GetDurationOfVideoFiles(List<string> filePaths)
-        //{
-        //    TimeSpan output = default;
-        //    Shell shell = new Shell();
-
-        //    foreach (string path in filePaths)
-        //    {
-        //        // http://www.levibotelho.com/development/get-the-length-of-a-video-in-c/
-        //        Folder folder = shell.NameSpace(path);
-        //        foreach (FolderItem2 item in folder.Items())
-        //        {
-        //            if (item.Name.EndsWith(".mp4"))
-        //            {
-        //                output += TimeSpan.FromSeconds(item.ExtendedProperty("System.Media.Duration") / 10000000);
-        //            }
-        //        }
-        //    }
-        //    return output;
-        //}    
+   
         public static TimeSpan GetDurationOfVideoFiles(string path)
         {
             TimeSpan output = default;
